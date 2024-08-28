9d67a60bf2e56256dba9fc4c346c50c0e033d9cd
Golden Phoenix <goldenfinix68>
2024-08-28 13:38:59 -0400

    Update app

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,62 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import "./App.css";
+import { CustomButton } from "./components/custom.button/CustomButton";
+import { CustomCheckbox } from "./components/custom.checkbox/CustomCheckbox";
 
 function App() {
+  const pages = ["Page 1", "Page 2", "Page 3", "Page 4"];
+  const [pageStatus, setPageStatus] = useState<Record<string, boolean>>(
+    pages.reduce((pv, cur) => {
+      return { ...pv, [cur]: false };
+    }, {})
+  );
+  const [isAllChecked, setAllChecked] = useState<boolean>(false);
+
+  const handleCheckbox = (name: string, checked: boolean) => {
+    if (name === "All pages") {
+      const newPageStatus = pages.reduce((pv, cur) => {
+        return { ...pv, [cur]: checked };
+      }, {});
+
+      setAllChecked(checked);
+      setPageStatus(newPageStatus);
+    } else {
+      const newPageStatus = { ...pageStatus, [name]: checked };
+      const checkedPages = pages.reduce(
+        (pv, cur) => (newPageStatus[cur] ? pv + 1 : pv),
+        0
+      );
+
+      const allChecked = checkedPages === pages.length ? true : false;
+
+      setAllChecked(allChecked);
+      setPageStatus(newPageStatus);
+    }
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="container">
+        <CustomCheckbox
+          title="All pages"
+          checked={isAllChecked}
+          setChecked={handleCheckbox}
+        />
+        <div className="line" />
+        {pages.map((val) => (
+          <CustomCheckbox
+            key={val}
+            title={val}
+            checked={pageStatus[val]}
+            setChecked={handleCheckbox}
+          />
+        ))}
+        <div className="line" />
+        <CustomButton
+          title="Done"
+          onClick={() => handleCheckbox("All pages", false)}
+        />
+      </div>
     </div>
   );
 }
